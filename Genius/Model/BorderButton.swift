c3739c58152e9f016393dfe9074383b19eb198e5
Dmitry <kashlevdmitry@gmail.com>
2019-02-23 23:33:09 +0300

    new buttons

--- Genius/Model/BorderButton.swift
+++ Genius/Model/BorderButton.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class BorderButton: UIButton {
     
-    override func awakeFromNib() {
-        
-        super.awakeFromNib()
-        
-        layer.borderWidth  =  2.0
-        layer.borderColor  =  UIColor.white.cgColor
-        layer.cornerRadius =  30
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
     }
     
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    
+    func setupButton() {
+        
+        setShadow()
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font     = UIFont(name: "AvenirNext-DemiBold", size: 18)
+        layer.cornerRadius   = 20
+        layer.borderWidth    = 2.0
+        layer.borderColor    = UIColor.white.cgColor
+    }
+    
+    
+    private func setShadow() {
+        
+        layer.shadowColor   = UIColor.cyan.cgColor
+        layer.shadowOffset  = CGSize(width: 0.0, height: 5.0)
+        layer.shadowRadius  = 6
+        layer.shadowOpacity = 0.5
+        clipsToBounds       = true
+        layer.masksToBounds = false
+    }
 }
